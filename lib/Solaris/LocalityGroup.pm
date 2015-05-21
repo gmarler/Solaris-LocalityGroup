36ccbbdf948db1af3fb81bb8ec6ea880f767f8cd
Gordon Marler <gmarler@bloomberg.net>
2015-05-21 17:28:53 -0400

    attribute test passes

--- lib/Solaris/LocalityGroup.pm
+++ lib/Solaris/LocalityGroup.pm
@@ -1,6 +1,30 @@
 use strict;
 use warnings;
+use v5.18.1;
+use feature qw(say);
 
 package Solaris::LocalityGroup;
+
+# VERSION
+#
+use Moose;
+use Moose::Util::TypeConstraints;
+use MooseX::ClassAttribute;
+with 'MooseX::Log::Log4perl';
+
+use namespace::autoclean;
+
+use autodie                             qw(:all);
+use Readonly                            qw();
+
+
+Readonly::Scalar my $KSTAT  => '/bin/kstat';
+
+#
+# Instance Attributes
+#
+# Locality Group Type
+has 'type'      => ( isa => enum([ qw( root leaf ) ]), is => 'ro', required => 1 );
+
 
 1;
